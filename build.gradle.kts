2943b1a5a857a1ed26088e36a45ad55d279d72df
h-yoshikawa44 <43331308+h-yoshikawa44@users.noreply.github.com>
2022-10-23 19:43:15 +0900

    ch6-3. プロジェクトの環境構築 - build.gradle.ktsへの依存関係の追加
    mybatis-generatorとGradle 7系の相性問題があるので、6系に落とした。
    Javaバージョンも併せて15に落とした。

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,13 +3,14 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 plugins {
 	id("org.springframework.boot") version "2.7.5"
 	id("io.spring.dependency-management") version "1.0.15.RELEASE"
+	id("com.arenagod.gradle.MybatisGenerator") version "1.4"
 	kotlin("jvm") version "1.6.21"
 	kotlin("plugin.spring") version "1.6.21"
 }
 
 group = "com.book.manager"
 version = "0.0.1-SNAPSHOT"
-java.sourceCompatibility = JavaVersion.VERSION_17
+java.sourceCompatibility = JavaVersion.VERSION_15
 
 repositories {
 	mavenCentral()
@@ -20,17 +21,30 @@ dependencies {
 	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
 	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+
 	implementation("org.mybatis.spring.boot:mybatis-spring-boot-starter:2.2.2")
+	implementation("org.mybatis.dynamic-sql:mybatis-dynamic-sql:1.4.1")
+	implementation("mysql:mysql-connector-java:8.0.30")
+	mybatisGenerator("org.mybatis.generator:mybatis-generator-core:1.4.1")
+
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 }
 
 tasks.withType<KotlinCompile> {
 	kotlinOptions {
 		freeCompilerArgs = listOf("-Xjsr305=strict")
-		jvmTarget = "17"
+		jvmTarget = "15"
 	}
 }
 
 tasks.withType<Test> {
 	useJUnitPlatform()
+}
+
+mybatisGenerator {
+	verbose = true
+	configFile = "${projectDir}/src/main/resources/generatorConfig.xml"
+	dependencies {
+		mybatisGenerator(group="mysql", name="mysql-connector-java", version="8.0.30")
+	}
 }
