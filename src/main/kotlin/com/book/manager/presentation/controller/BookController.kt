f36911272ac89b1b6641477477ab16f968017d80
h-yoshikawa44 <43331308+h-yoshikawa44@users.noreply.github.com>
2022-10-29 20:05:53 +0900

    ch6-4. 検索系機能（一覧取得、詳細取得）のAPI実装 - 一覧 - Controllerの実装

---
+++ src/main/kotlin/com/book/manager/presentation/controller/BookController.kt
@@ -0,0 +1,26 @@
+package com.book.manager.presentation.controller
+
+import com.book.manager.application.service.BookService
+import com.book.manager.presentation.form.BookInfo
+import com.book.manager.presentation.form.GetBookListResponse
+import org.springframework.web.bind.annotation.CrossOrigin
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+// API のルーティングや、クライアントからのパラメータをつけ取って Service のロジックを実行する層
+@RestController
+@RequestMapping("book")
+@CrossOrigin
+class BookController(
+    private val bookService: BookService
+) {
+    @GetMapping("/list")
+    fun getList(): GetBookListResponse {
+        val bookList = bookService.getList().map {
+            BookInfo(it)
+        }
+        // 最終的に、モデルクラスをレスポンスクラス型に変換して返す
+        return GetBookListResponse(bookList)
+    }
+}
