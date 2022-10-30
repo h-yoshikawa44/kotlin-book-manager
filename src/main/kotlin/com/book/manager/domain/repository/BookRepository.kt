10a794a1877dc8028c5395cb41219ad1aa76b04f
h-yoshikawa44 <43331308+h-yoshikawa44@users.noreply.github.com>
2022-10-30 17:35:46 +0900

    ch6-4. 検索系機能（一覧取得、詳細取得）のAPI実装 - 詳細 - Repositoryの実装

--- src/main/kotlin/com/book/manager/domain/repository/BookRepository.kt
+++ src/main/kotlin/com/book/manager/domain/repository/BookRepository.kt
@@ -4,4 +4,6 @@ import com.book.manager.domain.model.BookWithRentalModel
 
 interface BookRepository {
     fun findAllWithRental(): List<BookWithRentalModel>
+
+    fun findWithRental(id: Long): BookWithRentalModel?
 }
--- src/main/kotlin/com/book/manager/infrastructure/database/repository/BookRepositoryImpl.kt
+++ src/main/kotlin/com/book/manager/infrastructure/database/repository/BookRepositoryImpl.kt
@@ -6,6 +6,7 @@ import com.book.manager.domain.model.RentalModel
 import com.book.manager.domain.repository.BookRepository
 import com.book.manager.infrastructure.database.mapper.custom.BookWithRentalMapper
 import com.book.manager.infrastructure.database.mapper.custom.select
+import com.book.manager.infrastructure.database.mapper.custom.selectByPrimaryKey
 import org.springframework.stereotype.Repository
 
 // interface を使用した上で実装することで、DB 関連の実装をこの中に閉じ込め、呼出し層から意識する必要がなくなる
@@ -17,6 +18,11 @@ class BookRepositoryImpl(
     override fun findAllWithRental(): List<BookWithRentalModel> {
         // Record クラスはあくまで MyBatis に依存するため、Model クラスに変換することで、呼出し側からは Model クラスだけ意識でよくなる
         return bookWithRentalMapper.select().map { toModel(it) }
+    }
+
+    override fun findWithRental(id: Long): BookWithRentalModel? {
+        // 安全呼び出しと let の組み合わせにより、データを取得できなかった場合は null を返却
+        return bookWithRentalMapper.selectByPrimaryKey(id)?.let { toModel(it) }
     }
 
     private fun toModel(record: com.book.manager.infrastructure.database.record.custom.BookWithRental): BookWithRentalModel {
