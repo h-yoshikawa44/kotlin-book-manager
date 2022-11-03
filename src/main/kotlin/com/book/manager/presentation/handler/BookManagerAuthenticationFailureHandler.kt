1346f2cc24b844e296f3b5b8a28c609d4f34e18d
h-yoshikawa44 <43331308+h-yoshikawa44@users.noreply.github.com>
2022-11-03 23:05:13 +0900

    ch7-1. Spring Securityでユーザ認証、認可の機構を実装する - 認証失敗時のハンドラー

---
+++ src/main/kotlin/com/book/manager/presentation/handler/BookManagerAuthenticationFailureHandler.kt
@@ -0,0 +1,17 @@
+package com.book.manager.presentation.handler
+
+import org.springframework.security.core.AuthenticationException
+import org.springframework.security.web.authentication.AuthenticationFailureHandler
+import javax.servlet.http.HttpServletRequest
+import javax.servlet.http.HttpServletResponse
+
+class BookManagerAuthenticationFailureHandler : AuthenticationFailureHandler {
+    // 認証失敗した場合の処理を記述
+    override fun onAuthenticationFailure(
+        request: HttpServletRequest,
+        response: HttpServletResponse,
+        exception: AuthenticationException
+    ) {
+        response.status = HttpServletResponse.SC_UNAUTHORIZED
+    }
+}
