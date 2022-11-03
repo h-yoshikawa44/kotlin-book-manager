4c91bfafccb514821a6d72ef50e2ff4d03289c3a
h-yoshikawa44 <43331308+h-yoshikawa44@users.noreply.github.com>
2022-11-03 23:20:48 +0900

    フロント側もパスワードはpassでなくpasswordにした

--- front/src/api/auth.js
+++ front/src/api/auth.js
@@ -4,7 +4,7 @@ export default {
   login (email, password) {
     var params = new URLSearchParams()
     params.append('email', email)
-    params.append('pass', password)
+    params.append('password', password)
 
     return client.post('http://localhost:8080/login', params)
   }
