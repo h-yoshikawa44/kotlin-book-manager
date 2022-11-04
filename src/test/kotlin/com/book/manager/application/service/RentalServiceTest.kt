2d15e249fc69806a41b74de0f752a6d6a54be058
h-yoshikawa44 <43331308+h-yoshikawa44@users.noreply.github.com>
2022-11-04 16:36:36 +0900

    ch8-2. JUnitでWebアプリケーションの単体テスト - モック化とテスト - 例外スローの検証

--- src/test/kotlin/com/book/manager/application/service/RentalServiceTest.kt
+++ src/test/kotlin/com/book/manager/application/service/RentalServiceTest.kt
@@ -8,11 +8,12 @@ import com.book.manager.domain.model.UserModel
 import com.book.manager.domain.repository.BookRepository
 import com.book.manager.domain.repository.RentalRepository
 import com.book.manager.domain.repository.UserRepository
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Assertions
+
+
 import org.junit.jupiter.api.Test
-import org.mockito.kotlin.any
-import org.mockito.kotlin.mock
-import org.mockito.kotlin.verify
-import org.mockito.kotlin.whenever
+import org.mockito.kotlin.*
 import java.time.LocalDate
 import java.time.LocalDateTime
 
@@ -41,5 +42,29 @@ internal class RentalServiceTest {
         verify(userRepository).find(userId)
         verify(bookRepository).findWithRental(bookId)
         verify(rentalRepository).endRental(bookId)
+    }
+
+    // 異常系のテスト
+    @Test
+    fun `endRental when book is not rental then throw exception`() {
+        val userId = 100L
+        val bookId = 1000L
+        val user = UserModel(userId, "test@test.com", "pass", "kotlin", RoleType.USER)
+        val book = BookModel(bookId, "Kotlin入門", "コトリン太郎", LocalDate.now())
+        val bookWithRental = BookWithRentalModel(book, null)
+
+        whenever(userRepository.find(any() as Long)).thenReturn(user)
+        whenever(bookRepository.findWithRental(any())).thenReturn(bookWithRental)
+
+        // 例外スローの検証
+        val exception = Assertions.assertThrows(IllegalStateException::class.java) {
+            rentalService.endRental(bookId, userId)
+        }
+
+        assertThat(exception.message).isEqualTo("未貸出の商品です bookId:${bookId}")
+
+        verify(userRepository).find(userId)
+        verify(bookRepository).findWithRental(bookId)
+        verify(rentalRepository, times(0)).endRental(any())
     }
 }
