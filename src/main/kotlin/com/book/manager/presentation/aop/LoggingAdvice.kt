6ef38dd4e2b16c092f2b19670a7e412236831a02
h-yoshikawa44 <43331308+h-yoshikawa44@users.noreply.github.com>
2022-11-04 14:40:14 +0900

    ch7-3. Spring AOPでログ出力 - @Before、@AfterでControllerの前後にロギングを追加

---
+++ src/main/kotlin/com/book/manager/presentation/aop/LoggingAdvice.kt
@@ -0,0 +1,35 @@
+package com.book.manager.presentation.aop
+
+import com.book.manager.application.service.security.BookManagerUserDetails
+import org.aspectj.lang.JoinPoint
+import org.aspectj.lang.annotation.After
+import org.aspectj.lang.annotation.Aspect
+import org.aspectj.lang.annotation.Before
+import org.slf4j.LoggerFactory
+import org.springframework.security.core.context.SecurityContextHolder
+import org.springframework.stereotype.Component
+import org.springframework.web.context.request.RequestContextHolder
+import org.springframework.web.context.request.ServletRequestAttributes
+
+// SLF4J ロガーの生成
+private val logger = LoggerFactory.getLogger(LoggingAdvice::class.java)
+
+@Aspect
+@Component
+class LoggingAdvice {
+    // 前処理定義
+    @Before("execution(* com.book.manager.presentation.controller..*.*(..))")
+    fun beforeLog(joinPoint: JoinPoint) {
+        val user = SecurityContextHolder.getContext().authentication.principal as BookManagerUserDetails
+        logger.info("Start: ${joinPoint.signature} userId=${user.id}")
+        logger.info("Class: ${joinPoint.target.javaClass}")
+        logger.info("Session: ${(RequestContextHolder.getRequestAttributes() as ServletRequestAttributes).request.session.id}")
+    }
+
+    // 後処理定義
+    @After("execution(* com.book.manager.presentation.controller..*.*(..))")
+    fun afterLog(joinPoint: JoinPoint) {
+        val user = SecurityContextHolder.getContext().authentication.principal as BookManagerUserDetails
+        logger.info("End: ${joinPoint.signature} userId=${user.id}")
+    }
+}
