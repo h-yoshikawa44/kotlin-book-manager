cc6d33dd568a72d75f6726eecbfb38f45d7f287a
h-yoshikawa44 <43331308+h-yoshikawa44@users.noreply.github.com>
2022-11-04 13:54:27 +0900

    ch7-1. Spring Securityでユーザ認証、認可の機構を実装する - セッション情報の保存先をRedisへ向ける設定クラスを作成

---
+++ src/main/kotlin/com/book/manager/presentation/config/HttpSessionConfig.kt
@@ -0,0 +1,20 @@
+package com.book.manager.presentation.config
+
+import org.springframework.context.annotation.Bean
+import org.springframework.data.redis.connection.jedis.JedisConnectionFactory
+import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession
+
+@EnableRedisHttpSession
+class HttpSessionConfig {
+    @Bean
+    fun connectionFactory(): JedisConnectionFactory {
+        // val redisStandaloneConfiguration = RedisStandaloneConfiguration().also {
+        //     it.hostName = "kotlin-redis"
+        //     it.port = 16379
+        // }
+        // return JedisConnectionFactory(redisStandaloneConfiguration)
+
+        // Bean 定義し、JedisConnectionFactory のインスタンスを返すことで DI で使用できるようにする
+        return JedisConnectionFactory()
+    }
+}
