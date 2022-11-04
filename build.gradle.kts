3c3f34ca763a02a5284701ea944ff4e6a6c01cc2
h-yoshikawa44 <43331308+h-yoshikawa44@users.noreply.github.com>
2022-11-04 14:31:55 +0900

    ch7-3. Spring AOPでログ出力 - build.gradle.ktsへの依存関係の追加

--- build.gradle.kts
+++ build.gradle.kts
@@ -29,6 +29,7 @@ dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-security")
 	implementation("org.springframework.session:spring-session-data-redis")
 	implementation("redis.clients:jedis")
+	implementation("org.springframework.boot:spring-boot-starter-aop")
 
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 }
