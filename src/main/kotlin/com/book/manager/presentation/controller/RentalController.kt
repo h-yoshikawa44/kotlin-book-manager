27cd8bdbf9a182c6d4863efce45ef234fd6dfa98
h-yoshikawa44 <43331308+h-yoshikawa44@users.noreply.github.com>
2022-11-04 14:27:29 +0900

    ch7-2. 貸出、返却機能のAPI実装 - 返却 - Controllerの実装

--- src/main/kotlin/com/book/manager/presentation/controller/RentalController.kt
+++ src/main/kotlin/com/book/manager/presentation/controller/RentalController.kt
@@ -4,6 +4,8 @@ import com.book.manager.application.service.RentalService
 import com.book.manager.application.service.security.BookManagerUserDetails
 import com.book.manager.presentation.form.RentalStartRequest
 import org.springframework.security.core.context.SecurityContextHolder
+import org.springframework.web.bind.annotation.DeleteMapping
+import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
@@ -19,5 +21,11 @@ class RentalController(
         // SecurityContextHolder の中に認証したユーザの情報が保持されている
         val user = SecurityContextHolder.getContext().authentication.principal as BookManagerUserDetails
         rentalService.startRental(request.bookId, user.id)
+    }
+
+    @DeleteMapping("/end/{book_id}")
+    fun endRental(@PathVariable("book_id") bookId: Long) {
+        val user = SecurityContextHolder.getContext().authentication.principal as BookManagerUserDetails
+        rentalService.endRental(bookId, user.id)
     }
 }
